75b3d68245af57d49ba81558ba3ef375b22610d7
sd <hermans.paul@ziggo.nl>
2022-02-07 19:12:15 +0100

    Version number change, test response on request

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -284,28 +284,24 @@ void LogClean(AsyncWebServerRequest *request) {
 }
 
 void ringChime(AsyncWebServerRequest *request) {
-    String msg = F("Ring request received");
-    msg += F("\n");
+    String msg = F("Ring request received \n");
     AddLogMessageI(msg);
-    String s = F("<h1>");
-    s += esp_name;
-    s += F("</h1><p>Ring request received</p>");
+    //String s = F("<h1>");
+    //s += esp_name;
+    //s += F("</h1><p>Ring request received</p>");
+    String s = F("Ring request received");
     request->send(200, "text/html", makePage(esp_name, s));
 
     HTTP_Received("On");
     if (strcmp(esp_board, "ESP_Wroom") == 0) {
-       //digitalWrite(13, HIGH);
        digitalWrite(PHOTOMOS_GPIO_Wroom, HIGH);
     } else {
-       //digitalWrite(22, HIGH);
        digitalWrite(PHOTOMOS_GPIO_M5_pico, HIGH);
     }
     RFsend(RFcode);
     if (strcmp(esp_board, "ESP_Wroom") == 0) {
-       //digitalWrite(13, LOW);
        digitalWrite(PHOTOMOS_GPIO_Wroom, LOW);
     } else {
-       //digitalWrite(22, LOW);
        digitalWrite(PHOTOMOS_GPIO_M5_pico, LOW);
     }
     if (!strcmp(SendOff, "yes")) {
